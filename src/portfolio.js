42d0e2e0ff03b293dc9de0c0335c6f173293e149
tyson-lin <tysonlin@umich.edu>
2026-05-07 20:39:11 -0500

    first edit

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,11 +20,9 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
-  ),
+  username: "Tyson Lin",
+  title: "Hi all, I'm Tyson",
+  subTitle: "A University of Michigan Computer Engineering graduate looking to build a career in embedded systems and software engineering",
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
